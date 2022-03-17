3a3dfbb49185ecdccf25f72e5f3c79429a0f6f60
Evan Hopkins <moviemaker7@gmail.com>
2022-03-17 17:06:49 -0400

    move example code to README

--- jules.test.ts
+++ jules.test.ts
@@ -8,135 +8,20 @@ enum Level {
     SENIOR="SENIOR",
     STANDARD="STANDARD"
 }
+type Person = {
+    age: number,
+    location: string,
+}
+
+const personA: Person = { age: 16, location: 'NJ'}
+const personB: Person = { age: 26, location: 'PA'}
+const personC: Person = { age: 42, location: 'NY'}
+const personD: Person = { age: 65, location: 'FL'}
 
 
-const engine1 = new Engine<{age: number, location: string}>([
+// Inline engine definition and test
+const result = Jules.engine<Person>([
     ({age}) => age >= 21 && age < 30,
     ({age}) => age >= 30 && age < 60,
     ({age, location}) => age >= (location === 'FL' ? 55 : 60),
-])
-
-const result1 = engine1.run({age: 26, location: 'PA'})
-// => 0
-
-const engine2 = new Engine<{age: number, location: string}>([
-    {
-        id: 'JUNIOR',
-        test: ({age}) => age >= 21 && age < 30
-    },
-    {
-        id: 'STANDARD',
-        test: ({age}) => age >= 30 && age < 60
-    },
-    { 
-        id: 'SENIOR',
-        test: ({age, location}) => age >= (location === 'FL' ? 55 : 60)
-    },
-])
-const result2 = engine2.run({age: 26, location: 'PA'})
-// => JUNIOR
-
-const engine3 = new Engine<{age: number, location: string}>([
-    {
-        id: 'JUNIOR',
-        test: ({age}) => age >= 21 && age < 30,
-        result: 'Junior Membership'
-    },
-    {
-        id: 'STANDARD',
-        test: ({age}) => age >= 30 && age < 60,
-        result: 'Standard Membership'
-    },
-    { 
-        id: 'SENIOR',
-        test: ({age, location}) => age >= (location === 'FL' ? 55 : 60),
-        result: 'Senior Membership'
-
-    },
-])
-
-const result3 = engine3.run({age: 26, location: 'PA'})
-// => Junior Membership
-
-const engine4 = new Engine<{age: number, location: string}>([
-    {
-        id: 'JUNIOR',
-        test: ({age}) => age >= 21 && age < 30,
-        result: ({location}) => getBillingCode({location, level: Level.JUNIOR}),
-    },
-    {
-        id: 'STANDARD',
-        test: ({age}) => age >= 30 && age < 60,
-        result: ({location}) => getBillingCode({location, level: Level.STANDARD}),
-    },
-    { 
-        id: 'SENIOR',
-        test: ({age, location}) => age >= (location === 'FL' ? 55 : 60),
-        result: ({location}) => getBillingCode({location, level: Level.SENIOR}),
-    },
-])
-
-const result4 = engine4.run({age: 26, location: 'PA'})
-// => PA576
-
-const engine5 = new Engine<{age: number, location: string}>([
-    {
-        id: 'JUNIOR',
-        test: ({age}) => age >= 21 && age < 30,
-        result: ({location}) => getBillingCode({location, level: Level.JUNIOR}),
-        andNot: ['CALIFORNIA'],
-    },
-    {
-        id: 'STANDARD',
-        test: ({age}) => age >= 30 && age < 60,
-        result: ({location}) => getBillingCode({location, level: Level.STANDARD}),
-        andNot: ['CALIFORNIA'],
-    },
-    { 
-        id: 'SENIOR',
-        test: ({age, location}) => age >= (location === 'FL' ? 55 : 60),
-        result: ({location}) => getBillingCode({location, level: Level.SENIOR}),
-        andNot: ['CALIFORNIA'],
-    },
-    { 
-        id: 'CALIFORNIA',
-        test: ({location}) => location === 'CA',
-        result: ({location}) => getBillingCode({location, level: Level.CALIFORNIA}),
-    },
-])
-
-const result5 = engine5.run({age: 26, location: 'CA'})
-// => CA102
-
-
-const engine6 = new Engine<{age: number, location: string}>({
-    facts: [
-        {id: 'IN_US', test: ({location}) => states.includes(location)},
-    ],
-    rules: [
-        {
-            id: 'JUNIOR',
-            test: ({age}, facts) => age >= 21 && age < 30,
-            andNot: ['CALIFORNIA'],
-            and: ['IN_US'],// you can simply refence rules in and / andNots
-        },
-        {
-            id: 'STANDARD',
-            test: ({age}, facts) => age >= 30 && age < 60 && facts['IN_US'],// you also have access to facts in your test fn
-            andNot: ['CALIFORNIA'],
-        },
-        { 
-            id: 'SENIOR',
-            test: ({age, location}, facts) => age >= (location === 'FL' ? 55 : 60),
-            andNot: ['CALIFORNIA'],
-            and: ['IN_US'],
-        },
-        { 
-            id: 'CALIFORNIA',
-            test: ({location}, facts) => location === 'CA',
-        },
-    ]
-})
-
-const result6 = engine5.run({age: 26, location: 'Cabo'})
-// => undefined
+]).expect(RESULT.ONE).test(personA)
